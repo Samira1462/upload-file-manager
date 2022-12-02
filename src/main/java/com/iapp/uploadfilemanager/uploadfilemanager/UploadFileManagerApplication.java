4d0b757ac72e7a62928fc895402d83be7e3fdec2
samira <Samir.Radmaneshfar@gmail.com>
2022-12-02 17:03:18 +0100

    first commit

---
+++ src/main/java/com/iapp/uploadfilemanager/uploadfilemanager/UploadFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iapp.uploadfilemanager.uploadfilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iapp/uploadfilemanager/uploadfilemanager/UploadFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iapp.uploadfilemanager.uploadfilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
